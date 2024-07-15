089f58cd401986e5569133c53b6b4725180bdc22
Abrahul-107 <Abrahul-107>
2024-07-15 23:03:17 +0530

    Thread creation from Runnable class

---
+++ MultithreadingAndConcurrency/ThreadFromRunnable/FromRunnable.java
@@ -0,0 +1,12 @@
+package MultithreadingAndConcurrency.ThreadFromRunnable;
+
+public class FromRunnable implements Runnable
+{
+    @Override
+    public void run()
+    {
+        for(int i=0;i<5;i++)
+            System.out.println(Thread.currentThread()+"Inside runnable "+i+1);
+    }
+    
+}
